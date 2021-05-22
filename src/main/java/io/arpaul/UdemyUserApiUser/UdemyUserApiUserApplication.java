fd0bd6c319de4e2ecf00e6335ff0e7d1cef5bd71
Aritra1704 <aritra1704@gmail.com>
2021-05-22 19:05:53 +0530

    Initial commit

---
+++ src/main/java/io/arpaul/UdemyUserApiUser/UdemyUserApiUserApplication.java
@@ -0,0 +1,13 @@
+package io.arpaul.UdemyUserApiUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyUserApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyUserApiUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/arpaul/UdemyUserApiUser/UdemyUserApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.arpaul.UdemyUserApiUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyUserApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
