a8fad289d2882a5e9b2bdac7ed6617568625eed6
Aritra1704 <aritra1704@gmail.com>
2021-05-26 14:47:00 +0530

    Added response entity

--- src/main/java/io/arpaul/UdemyUserApiUser/controllers/UsersController.java
+++ src/main/java/io/arpaul/UdemyUserApiUser/controllers/UsersController.java
@@ -6,6 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,12 +34,12 @@ public class UsersController {
     }
     
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+    public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
     	ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
     	UserDto userDto = mapper.map(userDetails, UserDto.class);
     	userService.createUser(userDto);
-        return "Working on port "+env.getProperty("local.server.port");
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
