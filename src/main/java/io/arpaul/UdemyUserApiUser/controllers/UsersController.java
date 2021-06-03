bd4de9a00ebfef7cd91e3a4192dd8fa543e95151
Aritra1704 <aritra1704@gmail.com>
2021-06-03 13:29:08 +0530

    Resolved login api call issue

--- src/main/java/io/arpaul/UdemyUserApiUser/controllers/UsersController.java
+++ src/main/java/io/arpaul/UdemyUserApiUser/controllers/UsersController.java
@@ -34,10 +34,10 @@ public class UsersController {
         return "Working on port "+env.getProperty("local.server.port");
     }
 
-	@PostMapping("/login")
-	public String login() {
-		return "Working on port "+env.getProperty("local.server.port");
-	}
+//	@PostMapping("/login")
+//	public String login() {
+//		return "Working on port "+env.getProperty("local.server.port");
+//	}
     
     @PostMapping
     public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
