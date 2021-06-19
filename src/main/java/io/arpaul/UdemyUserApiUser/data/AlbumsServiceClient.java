5dc825518526433cc8fe19fa5c278e68019977c9
Aritra1704 <aritra1704@gmail.com>
2021-06-19 15:14:52 +0530

    Added Feign client factory implementation

--- src/main/java/io/arpaul/UdemyUserApiUser/data/AlbumsServiceClient.java
+++ src/main/java/io/arpaul/UdemyUserApiUser/data/AlbumsServiceClient.java
@@ -3,25 +3,61 @@ package io.arpaul.UdemyUserApiUser.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import feign.FeignException;
 import io.arpaul.UdemyUserApiUser.ui.model.AlbumResponseModel;
 
-@FeignClient(name="udemy-albums", fallback = AlbumsFallback.class)
+@FeignClient(name="udemy-albums", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
 	@GetMapping("/users/{id}/albums")
 	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
 
 @Component
-class AlbumsFallback implements AlbumsServiceClient {
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
 
 	@Override
+	public AlbumsServiceClient create(Throwable cause) {
+		return new ALbumsServiceClientFallback(cause);
+	}
+	
+}
+
+class ALbumsServiceClientFallback implements AlbumsServiceClient {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Throwable cause;
+	
+	public ALbumsServiceClientFallback(Throwable cause) {
+		this.cause = cause;
+	}
+	
+	@Override
 	public List<AlbumResponseModel> getAlbums(String id) {
+		if(cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+			logger.error("404 error took place when getAlbums was called with userId: "
+					+id+", Error message: "
+					+cause.getLocalizedMessage());
+		} else {
+			logger.error("Other error took place: "+cause.getLocalizedMessage());
+		}
 		return new ArrayList<>();
 	}
 	
 }
+//@Component
+//class AlbumsFallback implements AlbumsServiceClient {
+//
+//	@Override
+//	public List<AlbumResponseModel> getAlbums(String id) {
+//		return new ArrayList<>();
+//	}
+//	
+//}
